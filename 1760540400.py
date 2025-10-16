4977c5337134b11da49a641f7883d3ed16daa64a
Takuro Kitahara <kitaharata@outlook.jp>
2025-10-17 00:00:12 +0900

    Update 2 files

---
+++ 1760540400.py
@@ -0,0 +1,12 @@
+from waitress import serve
+
+
+def wsgiapp(environ, start_response):
+    status = "200 OK"
+    headers = [("Content-Type", "text/plain; charset=utf-8")]
+    start_response(status, headers)
+    return [b"Hello, World"]
+
+
+if __name__ == "__main__":
+    serve(wsgiapp, host="127.0.0.1", port=8080)
